713efe23f874a30b1d52e76194ce682005f1e7a4
Marcos Nolasco <marcosabn@gmail.com>
2021-10-10 17:45:21 -0300

    init scrapy tutorial

---
+++ tutorial/spiders/scraper.py
@@ -0,0 +1,22 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "quotes"  # identifies the Spyder
+
+    def start_requests(self):
+        """ return an iterable of Requests """
+        urls = [
+            'http://quotes.toscrape.com/page/1/',
+            'http://quotes.toscrape.com/page/2/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        """ method called to handle the response for each Request """
+        page = response.url.split("/")[-2]
+        filename = f'quotes-{page}.html'
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log(f'Saved file {filename}')
